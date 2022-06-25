659ffad8c48f83f8580b332bcc9ce051de98ed9f
singuyenmai <singuyenthumai@gmail.com>
2022-06-25 11:48:40 +0100

    [Rational_initMono] updated get_features func

--- Rational_initMono/main.sh
+++ Rational_initMono/main.sh
@@ -14,16 +14,16 @@ function get_features(){
     metafile=$collection"/metadata.tsv";
     output=$collection"/features.tsv";
 
-    echo -e exp_ID"\t"alpha_EZ"\t"alpha_ZE"\t"t5p"\t"t5p_first"\t"tTiny"\t"tTiny_first > $output;
+    echo -e exp_ID"\t"alpha_EZ"\t"alpha_ZE"\t"e_return"\t"t5p"\t"t5p_first"\t"tTiny"\t"tTiny_first"\t"total_drug_in > $output;
 
     while IFS=$'\t' read -r ID aEZ aZE || [ -n "$ID" ]; do
         pf=$collection"/"$ID"/testing_perf."$ID".tsv";
         awk -v a1=$aEZ -v a2=$aZE '{FS="\t"; OFS="\t"} FNR == 2 { \
-            x=$2; gsub(/[\[\]]/, "", x); n=split(x, t, " ");  \
-            y=$3; gsub(/[\[\]]/, "", y); nT=split(y, tT, " "); \
+            x=$3; gsub(/[\[\]]/, "", x); n=split(x, t, " ");  \
+            y=$4; gsub(/[\[\]]/, "", y); nT=split(y, tT, " "); \
             if (n > 0) pt=t[1]; else pt="N/A"; \
             if (nT > 0) ptT=tT[1]; else ptT="N/A"; \
-            print $1, a1, a2, $2, pt, $3, ptT}' $pf >> $output;
+            print $1, a1, a2, $2, $3, pt, $4, ptT, $5}' $pf >> $output;
     done < <(tail -n +2 $metafile);
 }
 export -f get_features
