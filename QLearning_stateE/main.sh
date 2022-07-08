ea82e57bd324cf6691e14602cb95839fcb0cb3f0
singuyenmai <singuyenthumai@gmail.com>
2022-07-08 15:35:16 +0100

    [QLearning_statE] rerun on local for training & policy fig

--- QLearning_stateE/main.sh
+++ QLearning_stateE/main.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-PARALLEL_SCRIPT="/home/s4278836/polyInfect/parallel_experiments.py"
+# PARALLEL_SCRIPT="/home/s4278836/polyInfect/parallel_experiments.py"
 
-python $PARALLEL_SCRIPT -f collection_params.qlearning.micEZ70.json
+# python $PARALLEL_SCRIPT -f collection_params.qlearning.micEZ70.json
 
-python $PARALLEL_SCRIPT -f collection_params.qlearning.micZ140.json
+# python $PARALLEL_SCRIPT -f collection_params.qlearning.micZ140.json
+
+PARALLEL_SCRIPT="../parallel_experiments.py"
+
+python $PARALLEL_SCRIPT -f collection_params.qlearning.micEZ70.json --local --re_test
+
+python $PARALLEL_SCRIPT -f collection_params.qlearning.micZ140.json --local --re_test
